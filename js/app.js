573cc9e18a27418fbb5c4cb4aaed1eca5c523b3f
Sebastien <srenault.contact@gmail.com>
2018-01-28 15:45:35 +0100

    Add upstairs pano

--- js/app.js
+++ js/app.js
@@ -20,12 +20,26 @@ const rdc = [{
   panorama: 'PANO_20180124_131634.jpg',
 }];
 
+const etage = [{
+  id: 'etage-ch1',
+  title: 'Chambre 1',
+  panorama: 'PANO_20180128_125521.jpg',
+}, {
+  id: 'etage-ch2',
+  title: 'Chambre 2',
+  panorama: 'PANO_20180128_125129.jpg',
+}, {
+  id: 'etage-sdb-ch3',
+  title: 'Salle de bain + Chambre 3',
+  panorama: 'PANO_20180128_124559.jpg',
+}];
+
 const defaultOptions = {
   type: 'equirectangular',
   autoLoad: true,
 };
 
-rdc.forEach(({ id, title, panorama }) => {
+rdc.concat(etage).forEach(({ id, title, panorama }) => {
   pannellum.viewer(id, Object.assign({
     panorama: `images/${panorama}`,
     title
